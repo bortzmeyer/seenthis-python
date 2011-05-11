d5fa6b2a261904e1179fc0e35a7c795a4ff3cb7b
Stephane Bortzmeyer <stephane+github@bortzmeyer.org>
2011-05-11 22:46:14 +0200

    TODO: launch an editor

--- seenthis-post.py
+++ seenthis-post.py
@@ -9,7 +9,9 @@ if len(sys.argv) != 1:
     print >>sys.stderr, ("Usage: %s\nThe message is read on the standard input" % \
                          sys.argv[0])
     sys.exit(1)
-    
+
+# TODO: read with a timeout of zero: if nothing is retrieved, run an
+# editor to get the message
 message = sys.stdin.read()
 result = st.post(message)
 print result
