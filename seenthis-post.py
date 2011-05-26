fb44fcc1c0f40a8a40694ca13ae9d2b1ffb65975
Stephane Bortzmeyer <stephane+github@bortzmeyer.org>
2011-05-26 08:45:31 +0200

    Now works also interactively (detects if the std input is a terminal or not, and runs an editor if it is)

--- seenthis-post.py
+++ seenthis-post.py
@@ -2,6 +2,9 @@
 
 import SeenThis
 import sys
+import os
+import tempfile
+import subprocess
 
 st = SeenThis.Connection()
 
@@ -10,8 +13,31 @@ if len(sys.argv) != 1:
                          sys.argv[0])
     sys.exit(1)
 
-# TODO: read with a timeout of zero: if nothing is retrieved, run an
-# editor to get the message
-message = sys.stdin.read()
+# If the user redirected standard input from a file or a process
+if not sys.stdin.isatty():
+    message = sys.stdin.read()
+# else it is an interactive user
+else:
+    if os.environ['EDITOR']:
+        editor = os.environ['EDITOR']
+    else:
+        editor = 'vi'
+    tmpfile = tempfile.NamedTemporaryFile(delete=True)
+    try:
+        run_editor = subprocess.Popen([editor, tmpfile.name],
+                                      shell=False,
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE)
+        result = run_editor.stdout.readlines()
+        if result == []:
+            result = run_editor.stderr.readlines()
+        message = open(tmpfile.name).read()
+    except OSError:
+        print >>sys.stderr, ("Cannot run: \"%s %s\"" % (editor, tmpfile.name))
+        raise
+    tmpfile.close()
 result = st.post(message)
 print result
+
+
+
